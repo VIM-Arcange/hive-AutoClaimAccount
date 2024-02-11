68b8935ed8a93e9b94cd429e2944e4e7aa708336
Arcange <VIM-Arcange@users.noreply.github.com>
2024-02-11 20:39:28 +0100

    bug fix

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-async function sleep(ms) {
+function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
